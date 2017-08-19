4ef6537ddcdf7ddb4aa7e3cb58966d02b49a4a66
Hüseyin Demir <demirhuseyinn.94@gmail.om>
2017-08-19 13:54:22 +0300

    Login and Authorization Version 1.2

--- LoginForm/LoginForm/MainNavigationForm.cs
+++ LoginForm/LoginForm/MainNavigationForm.cs
@@ -14,6 +14,9 @@ namespace LoginForm
 {
     public partial class MainNavigationForm : Form
     {
+        IMEEntities IME = new IMEEntities();
+        Worker LoggedPerson = new Worker();
+        AuthorizationValue Value = new AuthorizationValue();
         public MainNavigationForm()
         {
             InitializeComponent();
@@ -21,16 +24,29 @@ namespace LoginForm
 
         private void lblQuotation_DoubleClick(object sender, EventArgs e)
         {
-            if (true)
+           
+            //Yetkli Kontrolü
+            var canEnterModule = from a in IME.AuthorizationValues
+                        where a.Workers == LoggedPerson
+                        where a.AuthorizationID == 1
+                        select a;
+            if (canEnterModule!=null)
             {
-
+                LoginForm.Quotation.Quotation quotation = new Quotation.Quotation();
+                quotation.Show();
+            }
+            else
+            {
+                MessageBox.Show("siktir git");
             }
         }
 
         private void MainNavigationForm_Load(object sender, EventArgs e)
         {
-            //Login olan kullanıcının ID'si üzerinden Kullanıcı Bilgileri ve yetkileri çekilecek burada
+            //Login olan kullanıcıyı çekilmeli
             label1.Text = WorkerApp.ID.ToString();
+            int PersonID = WorkerApp.ID;
+            LoggedPerson = IME.Workers.Where(wID => wID.WorkerID == PersonID).FirstOrDefault();
         }
     }
 }
