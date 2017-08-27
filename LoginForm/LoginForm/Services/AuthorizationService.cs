e7cfe0cdd72d3d3a3794924f0a2315aab2a138bc
Hüseyin Demir <demirhuseyinn.94@gmail.om>
2017-08-27 13:37:06 +0300

    Authorization Management Version 1.0

---
+++ LoginForm/LoginForm/Services/AuthorizationService.cs
@@ -0,0 +1,40 @@
+﻿using LoginForm.DataSet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginForm.Services
+{
+    class AuthorizationService
+    {
+        IMEEntities IMEDB = new IMEEntities();
+        public List<AuthorizationValue> GetAvailAuthorization(Worker Worker)
+        {
+
+
+            return IMEDB.AuthorizationValues.Where(a => !a.Workers.Any(w => w.WorkerID == Worker.WorkerID)).ToList();
+
+        }
+        public List<AuthorizationValue> GetUserAuthorization(Worker Worker)
+        {
+
+            return IMEDB.AuthorizationValues.Where(a => a.Workers.Any(w => w.WorkerID == Worker.WorkerID)).ToList();
+
+        }
+        public List<AuthorizationValue> Authorizations()
+        {
+
+            return IMEDB.AuthorizationValues.ToList();
+
+        }
+        public void AddNewAuthorization(AuthorizationValue NewAuthorization)
+        {
+
+            IMEDB.AuthorizationValues.Add(NewAuthorization);
+            
+            IMEDB.SaveChanges();
+        }
+    }
+}
