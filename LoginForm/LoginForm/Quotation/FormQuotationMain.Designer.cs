f774886b5829b606bb897709fc2067095ef3f9b1
Cem Genar <cemgenar43@gmail.com>
2017-10-11 17:21:55 +0300

    Design-QuotationMain

--- LoginForm/LoginForm/Quotation/FormQuotationMain.Designer.cs
+++ LoginForm/LoginForm/Quotation/FormQuotationMain.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridViewX1 = new DevComponents.DotNetBar.Controls.DataGridViewX();
             this.No = new DevComponents.DotNetBar.Controls.DataGridViewProgressBarXColumn();
@@ -39,16 +39,19 @@
             this.CustID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CustName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.datetimeEnd = new System.Windows.Forms.DateTimePicker();
+            this.datetimeStart = new System.Windows.Forms.DateTimePicker();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.btnAddQuotation = new System.Windows.Forms.Button();
+            this.btnNewQuotation = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewX1)).BeginInit();
             this.panel1.SuspendLayout();
@@ -67,6 +70,15 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 146F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1262, 673);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
@@ -84,14 +96,14 @@
             this.PreparedBy,
             this.CustID,
             this.CustName});
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewX1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewX1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridViewX1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridViewX1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
             this.dataGridViewX1.Location = new System.Drawing.Point(3, 149);
@@ -150,79 +162,104 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.datetimeEnd);
+            this.panel1.Controls.Add(this.datetimeStart);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button10);
             this.panel1.Controls.Add(this.button9);
             this.panel1.Controls.Add(this.button8);
             this.panel1.Controls.Add(this.button7);
-            this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.btnAddQuotation);
+            this.panel1.Controls.Add(this.btnNewQuotation);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1256, 140);
             this.panel1.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "End Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Start Date";
+            // 
+            // datetimeEnd
+            // 
+            this.datetimeEnd.CustomFormat = "dd-MM-yyyy";
+            this.datetimeEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datetimeEnd.Location = new System.Drawing.Point(89, 79);
+            this.datetimeEnd.Name = "datetimeEnd";
+            this.datetimeEnd.Size = new System.Drawing.Size(119, 24);
+            this.datetimeEnd.TabIndex = 19;
+            // 
+            // datetimeStart
+            // 
+            this.datetimeStart.CustomFormat = "dd-MM-yyyy";
+            this.datetimeStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datetimeStart.Location = new System.Drawing.Point(89, 25);
+            this.datetimeStart.Name = "datetimeStart";
+            this.datetimeStart.Size = new System.Drawing.Size(119, 24);
+            this.datetimeStart.TabIndex = 18;
+            // 
             // button3
             // 
             this.button3.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button3.Location = new System.Drawing.Point(215, 22);
+            this.button3.Location = new System.Drawing.Point(532, 9);
             this.button3.Name = "button3";
             this.button3.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button3.Size = new System.Drawing.Size(97, 100);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "2";
+            this.button3.Size = new System.Drawing.Size(100, 114);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Delete Quotation";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
             this.button2.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.Location = new System.Drawing.Point(112, 22);
+            this.button2.Location = new System.Drawing.Point(429, 9);
             this.button2.Name = "button2";
             this.button2.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button2.Size = new System.Drawing.Size(97, 100);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "1";
+            this.button2.Size = new System.Drawing.Size(97, 114);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Modify Quotation";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Image = global::LoginForm.Properties.Resources.btnBackground;
-            this.button10.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button10.Location = new System.Drawing.Point(936, 22);
-            this.button10.Name = "button10";
-            this.button10.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button10.Size = new System.Drawing.Size(97, 100);
-            this.button10.TabIndex = 9;
-            this.button10.Text = "9";
-            this.button10.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button9.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button9.Location = new System.Drawing.Point(833, 22);
+            this.button9.Location = new System.Drawing.Point(214, 25);
             this.button9.Name = "button9";
-            this.button9.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button9.Size = new System.Drawing.Size(97, 100);
-            this.button9.TabIndex = 8;
-            this.button9.Text = "8";
+            this.button9.Size = new System.Drawing.Size(77, 78);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "Refresh List";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
             this.button8.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button8.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button8.Location = new System.Drawing.Point(730, 22);
+            this.button8.Location = new System.Drawing.Point(1150, 9);
             this.button8.Name = "button8";
             this.button8.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button8.Size = new System.Drawing.Size(97, 100);
-            this.button8.TabIndex = 7;
+            this.button8.Size = new System.Drawing.Size(97, 114);
+            this.button8.TabIndex = 13;
             this.button8.Text = "7";
             this.button8.UseVisualStyleBackColor = true;
             // 
@@ -230,62 +267,62 @@
             // 
             this.button7.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button7.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button7.Location = new System.Drawing.Point(627, 22);
+            this.button7.Location = new System.Drawing.Point(1047, 9);
             this.button7.Name = "button7";
             this.button7.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button7.Size = new System.Drawing.Size(97, 100);
-            this.button7.TabIndex = 6;
+            this.button7.Size = new System.Drawing.Size(97, 114);
+            this.button7.TabIndex = 12;
             this.button7.Text = "6";
             this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Image = global::LoginForm.Properties.Resources.btnBackground;
-            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button6.Location = new System.Drawing.Point(524, 22);
-            this.button6.Name = "button6";
-            this.button6.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button6.Size = new System.Drawing.Size(97, 100);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "5";
-            this.button6.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
             this.button5.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button5.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button5.Location = new System.Drawing.Point(421, 22);
+            this.button5.Location = new System.Drawing.Point(841, 9);
             this.button5.Name = "button5";
             this.button5.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button5.Size = new System.Drawing.Size(97, 100);
-            this.button5.TabIndex = 4;
+            this.button5.Size = new System.Drawing.Size(97, 114);
+            this.button5.TabIndex = 6;
             this.button5.Text = "4";
             this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::LoginForm.Properties.Resources.btnBackground;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button6.Location = new System.Drawing.Point(944, 9);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
+            this.button6.Size = new System.Drawing.Size(97, 114);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
             this.button4.Image = global::LoginForm.Properties.Resources.btnBackground;
             this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button4.Location = new System.Drawing.Point(318, 22);
+            this.button4.Location = new System.Drawing.Point(738, 9);
             this.button4.Name = "button4";
             this.button4.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.button4.Size = new System.Drawing.Size(97, 100);
-            this.button4.TabIndex = 3;
+            this.button4.Size = new System.Drawing.Size(97, 114);
+            this.button4.TabIndex = 4;
             this.button4.Text = "3";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // btnAddQuotation
+            // btnNewQuotation
             // 
-            this.btnAddQuotation.Image = global::LoginForm.Properties.Resources.btnBackground;
-            this.btnAddQuotation.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnAddQuotation.Location = new System.Drawing.Point(9, 22);
-            this.btnAddQuotation.Name = "btnAddQuotation";
-            this.btnAddQuotation.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
-            this.btnAddQuotation.Size = new System.Drawing.Size(97, 100);
-            this.btnAddQuotation.TabIndex = 0;
-            this.btnAddQuotation.Text = "Add Quotation";
-            this.btnAddQuotation.UseVisualStyleBackColor = true;
-            this.btnAddQuotation.Click += new System.EventHandler(this.btnAddQuotation_Click);
+            this.btnNewQuotation.Image = global::LoginForm.Properties.Resources.btnBackground;
+            this.btnNewQuotation.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnNewQuotation.Location = new System.Drawing.Point(326, 9);
+            this.btnNewQuotation.Name = "btnNewQuotation";
+            this.btnNewQuotation.Padding = new System.Windows.Forms.Padding(8, 16, 8, 0);
+            this.btnNewQuotation.Size = new System.Drawing.Size(97, 114);
+            this.btnNewQuotation.TabIndex = 1;
+            this.btnNewQuotation.Text = "New Quotation";
+            this.btnNewQuotation.UseVisualStyleBackColor = true;
+            this.btnNewQuotation.Click += new System.EventHandler(this.btnNewQuotation_Click);
             // 
             // FormQuotationMain
             // 
@@ -299,6 +336,7 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewX1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -315,15 +353,18 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn PreparedBy;
         private System.Windows.Forms.DataGridViewTextBoxColumn CustID;
         private System.Windows.Forms.DataGridViewTextBoxColumn CustName;
-        private System.Windows.Forms.Button btnAddQuotation;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnNewQuotation;
+        private System.Windows.Forms.DateTimePicker datetimeStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker datetimeEnd;
+        private System.Windows.Forms.Button button9;
     }
 }
