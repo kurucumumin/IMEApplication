4fee23962988c437d198987928f2fa1742023945
Cem Genar <cemgenar43@gmail.com>
2017-10-16 17:37:06 +0300

    Role&Auth Design Edit

--- LoginForm/LoginForm/Management/FormRoleAuth.Designer.cs
+++ LoginForm/LoginForm/Management/FormRoleAuth.Designer.cs
@@ -32,16 +32,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.chcNewRole = new System.Windows.Forms.CheckBox();
             this.txtNewRoleName = new System.Windows.Forms.TextBox();
-            this.lbRoleList = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.clbAuthorizationList = new System.Windows.Forms.CheckedListBox();
             this.clbNewAuthorizations = new System.Windows.Forms.CheckedListBox();
+            this.lbRoleList = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbRole
@@ -90,26 +96,12 @@
             this.txtNewRoleName.Size = new System.Drawing.Size(210, 24);
             this.txtNewRoleName.TabIndex = 7;
             // 
-            // lbRoleList
-            // 
-            this.lbRoleList.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lbRoleList.DisplayMember = "roleName";
-            this.lbRoleList.FormattingEnabled = true;
-            this.lbRoleList.ItemHeight = 18;
-            this.lbRoleList.Location = new System.Drawing.Point(57, 32);
-            this.lbRoleList.Margin = new System.Windows.Forms.Padding(4);
-            this.lbRoleList.Name = "lbRoleList";
-            this.lbRoleList.Size = new System.Drawing.Size(293, 364);
-            this.lbRoleList.TabIndex = 8;
-            this.lbRoleList.ValueMember = "RoleID";
-            this.lbRoleList.SelectedIndexChanged += new System.EventHandler(this.lbRoleList_SelectedIndexChanged);
-            // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -145,41 +137,95 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Role Name";
             // 
-            // panel2
+            // tableLayoutPanel2
             // 
-            this.panel2.Controls.Add(this.clbAuthorizationList);
-            this.panel2.Controls.Add(this.lbRoleList);
-            this.panel2.Controls.Add(this.clbNewAuthorizations);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 150);
-            this.panel2.Margin = new System.Windows.Forms.Padding(0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1172, 448);
-            this.panel2.TabIndex = 1;
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel3, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel4, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 150);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1172, 448);
+            this.tableLayoutPanel2.TabIndex = 12;
             // 
             // clbAuthorizationList
             // 
-            this.clbAuthorizationList.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.clbAuthorizationList.CheckOnClick = true;
+            this.clbAuthorizationList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.clbAuthorizationList.FormattingEnabled = true;
-            this.clbAuthorizationList.Location = new System.Drawing.Point(358, 32);
+            this.clbAuthorizationList.Location = new System.Drawing.Point(16, 16);
             this.clbAuthorizationList.Margin = new System.Windows.Forms.Padding(4);
             this.clbAuthorizationList.Name = "clbAuthorizationList";
-            this.clbAuthorizationList.Size = new System.Drawing.Size(293, 365);
+            this.clbAuthorizationList.Size = new System.Drawing.Size(325, 416);
             this.clbAuthorizationList.TabIndex = 10;
             this.clbAuthorizationList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.clbAuthorizationList_MouseClick);
             // 
             // clbNewAuthorizations
             // 
-            this.clbNewAuthorizations.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.clbNewAuthorizations.CheckOnClick = true;
+            this.clbNewAuthorizations.Dock = System.Windows.Forms.DockStyle.Fill;
             this.clbNewAuthorizations.FormattingEnabled = true;
-            this.clbNewAuthorizations.Location = new System.Drawing.Point(802, 32);
+            this.clbNewAuthorizations.Location = new System.Drawing.Point(16, 16);
             this.clbNewAuthorizations.Margin = new System.Windows.Forms.Padding(4);
             this.clbNewAuthorizations.Name = "clbNewAuthorizations";
-            this.clbNewAuthorizations.Size = new System.Drawing.Size(293, 365);
+            this.clbNewAuthorizations.Size = new System.Drawing.Size(326, 416);
             this.clbNewAuthorizations.TabIndex = 11;
             this.clbNewAuthorizations.MouseClick += new System.Windows.Forms.MouseEventHandler(this.clbNewAuthorizations_MouseClick);
+            // 
+            // lbRoleList
+            // 
+            this.lbRoleList.DisplayMember = "roleName";
+            this.lbRoleList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbRoleList.FormattingEnabled = true;
+            this.lbRoleList.ItemHeight = 18;
+            this.lbRoleList.Location = new System.Drawing.Point(16, 16);
+            this.lbRoleList.Margin = new System.Windows.Forms.Padding(4);
+            this.lbRoleList.Name = "lbRoleList";
+            this.lbRoleList.Size = new System.Drawing.Size(325, 416);
+            this.lbRoleList.TabIndex = 8;
+            this.lbRoleList.ValueMember = "RoleID";
+            this.lbRoleList.SelectedIndexChanged += new System.EventHandler(this.lbRoleList_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbRoleList);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(16);
+            this.panel2.Size = new System.Drawing.Size(357, 448);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.clbAuthorizationList);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(357, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(16);
+            this.panel3.Size = new System.Drawing.Size(357, 448);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.clbNewAuthorizations);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(814, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(0);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(16);
+            this.panel4.Size = new System.Drawing.Size(358, 448);
+            this.panel4.TabIndex = 14;
             // 
             // FormRoleAuths
             // 
@@ -196,7 +242,10 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -207,12 +256,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox chcNewRole;
         private System.Windows.Forms.TextBox txtNewRoleName;
-        private System.Windows.Forms.ListBox lbRoleList;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.CheckedListBox clbAuthorizationList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.CheckedListBox clbNewAuthorizations;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lbRoleList;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckedListBox clbAuthorizationList;
+        private System.Windows.Forms.Panel panel4;
     }
 }
