52ab58a3202c4536672709e8edff2794eac2eed1
Cem Genar <cemgenar43@gmail.com>
2017-11-09 17:09:56 +0300

    tempChange

--- LoginForm/LoginForm/QuotationModule/FormQuotationMain.cs
+++ LoginForm/LoginForm/QuotationModule/FormQuotationMain.cs
@@ -8,13 +8,11 @@ namespace LoginForm.QuotationModule
 {
     public partial class FormQuotationMain : Form
     {
-        List<Quotation> quotationList;
-
         public FormQuotationMain()
         {
             InitializeComponent();
             datetimeEnd.Value = DateTime.Now.AddDays(-7);
-            bringQuotationList();
+            BringQuotationList();
         }
 
         private void btnNewQuotation_Click(object sender, EventArgs e)
@@ -23,104 +21,12 @@ namespace LoginForm.QuotationModule
             this.Hide();
             FormQuotationAdd quotationForm = new FormQuotationAdd();
             quotationForm.ShowDialog();
-            //deneme quotationForm = new deneme();
-            //quotationForm.Show();
             this.Show();
-        }
-
-        private void bringQuotationList()
-        {
-            IMEEntities IME = new IMEEntities();
-            bringQuotationList(DateTime.Today, DateTime.Today.AddDays(-7));
-        }
-        private void bringQuotationList(DateTime startDate, DateTime endDate)
-        {
-            IMEEntities IME = new IMEEntities();
-
-            //quotationList = IME.Quotations.Where(q => (q.StartDate >= endDate) && (q.StartDate <= startDate)).ToList();
-
-            var list = from q in IME.Quotations
-                       join c in IME.Customers on q.CustomerID equals c.ID
-                       where q.StartDate >= endDate && q.StartDate <= startDate
-                       select new
-                       {
-                           Date = q.StartDate,
-                           QuotationNo = q.QuotationNo,
-                           RFQ = q.RFQNo,
-                           CustomerName = c.c_name
-                       };
-
-            populateGrid(list.ToList());
-        }
-
-        private void populateGrid(List<object> list)
-        {
-            throw new NotImplementedException();
         }
 
         private void btnRefreshList_Click(object sender, EventArgs e)
         {
-            bringQuotationList(datetimeStart.Value, datetimeEnd.Value);
-            //populateGrid();
-        }
-
-        private void populateGrid<T>(List<T> queryable)
-        {
-            dgQuotation.DataSource = null;
-            dgQuotation.DataSource = queryable;
-        }
-
-        private void btnModifyQuotation_Click(object sender, EventArgs e)
-        {
-            ModifyQuotation();
-        }
-
-        private void FormQuotationMain_Load(object sender, EventArgs e)
-        {
-            bringQuotationList();
-        }
-
-        private void txtSearchText_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == Convert.ToChar(Keys.Enter))
-            {
-                if(cbSearch.SelectedItem != null && cbSearch.SelectedItem.ToString() == "QUOT NUMBER")
-                {
-                    IMEEntities IME = new IMEEntities();
-
-                    //quotationList = IME.Quotations.Where(q => (q.StartDate >= endDate) && (q.StartDate <= startDate)).ToList();
-
-                    var list = from q in IME.Quotations
-                               join c in IME.Customers on q.CustomerID equals c.ID
-                               where q.QuotationNo.Contains(txtSearchText.Text)
-                               select new
-                               {
-                                   Date = q.StartDate,
-                                   QuotationNo = q.QuotationNo,
-                                   CustomerName = c.c_name
-                               };
-
-                    populateGrid(list.ToList());
-                }
-            }
-        }
-
-        private void ModifyQuotation()
-        {
-            if (dgQuotation.CurrentRow != null)
-            {
-                string QuotationNo = dgQuotation.CurrentRow.Cells["QuotationNo"].Value.ToString();
-
-                IMEEntities IME = new IMEEntities();
-                Quotation quo = IME.Quotations.Where(q => q.QuotationNo == QuotationNo).FirstOrDefault();
-
-                FormQuotationAdd newForm = new FormQuotationAdd(quo);
-                newForm.ShowDialog();
-            }
-            else
-            {
-                MessageBox.Show("You did not chose any quotation.", "Warning!");
-            }
+            BringQuotationList(datetimeStart.Value, datetimeEnd.Value);
         }
 
         private void dgQuotation_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -149,7 +55,7 @@ namespace LoginForm.QuotationModule
                         IME.Quotations.Remove(quo);
                         IME.SaveChanges();
 
-                        bringQuotationList();
+                        BringQuotationList();
 
                         MessageBox.Show("Quotation is successfully deleted.", "Success!");
                     }
@@ -165,5 +71,98 @@ namespace LoginForm.QuotationModule
                 MessageBox.Show("You did not chose any quotation.", "Warning!");
             }
         }
+
+        private void btnModifyQuotation_Click(object sender, EventArgs e)
+        {
+            ModifyQuotation();
+        }
+
+        private void FormQuotationMain_Load(object sender, EventArgs e)
+        {
+            BringQuotationList();
+        }
+
+        private void txtSearchText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                if(cbSearch.SelectedItem != null && cbSearch.SelectedItem.ToString() == "QUOT NUMBER")
+                {
+                    IMEEntities IME = new IMEEntities();
+
+                    var list = from q in IME.Quotations
+                               join c in IME.Customers on q.CustomerID equals c.ID
+                               where q.QuotationNo.Contains(txtSearchText.Text)
+                               select new
+                               {
+                                   Date = q.StartDate,
+                                   QuotationNo = q.QuotationNo,
+                                   CustomerName = c.c_name
+                               };
+
+                    populateGrid(list.ToList());
+                }
+            }
+        }
+
+        private void ModifyQuotation()
+        {
+            if (dgQuotation.CurrentRow != null)
+            {
+                string QuotationNo = dgQuotation.CurrentRow.Cells["QuotationNo"].Value.ToString();
+                Quotation quo;
+
+                IMEEntities IME = new IMEEntities();
+                try
+                {
+                    quo = IME.Quotations.Where(q => q.QuotationNo == QuotationNo).FirstOrDefault();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                if (quo != null)
+                {
+                    FormQuotationAdd newForm = new FormQuotationAdd(quo);
+                    newForm.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("You did not chose any quotation.", "Warning!");
+            }
+        }
+
+        private void BringQuotationList()
+        {
+            IMEEntities IME = new IMEEntities();
+            BringQuotationList(DateTime.Today, DateTime.Today.AddDays(-7));
+        }
+
+        private void BringQuotationList(DateTime startDate, DateTime endDate)
+        {
+            IMEEntities IME = new IMEEntities();
+
+            var list = from q in IME.Quotations
+                       join c in IME.Customers on q.CustomerID equals c.ID
+                       where q.StartDate >= endDate && q.StartDate <= startDate
+                       select new
+                       {
+                           Date = q.StartDate,
+                           QuotationNo = q.QuotationNo,
+                           RFQ = q.RFQNo,
+                           CustomerName = c.c_name
+                       };
+
+            populateGrid(list.ToList());
+        }
+
+        private void populateGrid<T>(List<T> queryable)
+        {
+            dgQuotation.DataSource = null;
+            dgQuotation.DataSource = queryable;
+        }
+
     }
 }
