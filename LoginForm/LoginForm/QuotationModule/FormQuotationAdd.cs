16d2984532e69048c238c9837802334d8ed4b8d9
Mümin Kurucu <kurucumumin94@gmail.com>
2017-11-20 10:09:18 +0300

    4 hane

--- LoginForm/LoginForm/QuotationModule/FormQuotationAdd.cs
+++ LoginForm/LoginForm/QuotationModule/FormQuotationAdd.cs
@@ -168,9 +168,7 @@ namespace LoginForm.QuotationModule
                 try { txtContactNote.Text = c.CustomerWorker.Note.Note_name; } catch { }
                 try { txtCustomerNote.Text = c.Note.Note_name; } catch { }
                 try { txtAccountingNote.Text = IME.Notes.Where(a=>a.ID==c.customerAccountantNoteID).FirstOrDefault().Note_name; } catch { }
-                cbRep.SelectedValue = c.Worker.WorkerID;
-                cbCurrency.SelectedItem = cbCurrency.FindStringExact(c.CurrNameQuo);
-                cbCurrType.SelectedItem = cbCurrType.FindStringExact(c.CurrTypeQuo);
+                cbRep.SelectedIndex = c.Worker.WorkerID;
             }
         }
 
@@ -2521,51 +2519,51 @@ namespace LoginForm.QuotationModule
 
         private void dgQuotationAddedItems_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            int index = e.ColumnIndex;
-            decimal sayi;
-            if (index == 11 || index == 12 || index == 13 || index == 19 || index == 20 || index == 21 || index == 22 || index == 23)
-            {
-                for (int i = 0; i < dgQuotationAddedItems.RowCount; i++)
-                {
-                    if (dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString() != null && dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString() != "")
-                    {
-                        sayi = Decimal.Parse(dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString());
-                            if (sayi.ToString() == Math.Round(sayi, 4).ToString() && sayi.ToString() == round.ToString())
-                            {
-                                dgQuotationAddedItems.Rows[i].Cells[index].Value = round.ToString();
-                            }
-                            else
-                            {
-                                round = sayi;
-                                dgQuotationAddedItems.Rows[i].Cells[index].Value = round.ToString();
-                                sonuc = Math.Round(round, 4);
-                            }
-                    }
-                }
-            }
+            //int index = e.ColumnIndex;
+            //decimal sayi;
+            //if (index == 11 || index == 12 || index == 13 || index == 19 || index == 20 || index == 21 || index == 22 || index == 23)
+            //{
+            //    for (int i = 0; i < dgQuotationAddedItems.RowCount; i++)
+            //    {
+            //        if (dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString() != null && dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString() != "")
+            //        {
+            //            sayi = Decimal.Parse(dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString());
+            //                if (sayi.ToString() == Math.Round(sayi, 4).ToString())
+            //                {
+            //                    dgQuotationAddedItems.Rows[i].Cells[index].Value = round.ToString();
+            //                }
+            //                else
+            //                {
+            //                    round = sayi;
+            //                    dgQuotationAddedItems.Rows[i].Cells[index].Value = round.ToString();
+            //                    sonuc = Math.Round(round, 4);
+            //                }
+            //        }
+            //    }
+            //}
         }
 
         private void dgQuotationAddedItems_CellLeave(object sender, DataGridViewCellEventArgs e)
         {
-            int index = e.ColumnIndex;
-            decimal sayi;
-            if (index == 11 || index == 12 || index == 13 || index == 19 || index == 20 || index == 21 || index == 22 || index == 23)
-            {
-                for (int i = 0; i < dgQuotationAddedItems.RowCount; i++)
-                {
-                    if (dgQuotationAddedItems.Rows[i].Cells[index].Value != null && dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString() != "")
-                    {
-                        sayi = Decimal.Parse(dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString());
-                        if (sayi.ToString() == sonuc.ToString())
-                            dgQuotationAddedItems.Rows[i].Cells[index].Value = sayi.ToString();
-                        else
-                        {
-                            sonuc = Math.Round(round, 4);
-                            dgQuotationAddedItems.Rows[i].Cells[index].Value = sonuc.ToString();
-                        }
-                    }
-                }
-            }
+            //int index = e.ColumnIndex;
+            //decimal sayi;
+            //if (index == 11 || index == 12 || index == 13 || index == 19 || index == 20 || index == 21 || index == 22 || index == 23)
+            //{
+            //    for (int i = 0; i < dgQuotationAddedItems.RowCount; i++)
+            //    {
+            //        if (dgQuotationAddedItems.Rows[i].Cells[index].Value != null && dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString() != "")
+            //        {
+            //            sayi = Decimal.Parse(dgQuotationAddedItems.Rows[i].Cells[index].Value.ToString());
+            //            if (sayi.ToString() == sonuc.ToString())
+            //                dgQuotationAddedItems.Rows[i].Cells[index].Value = sayi.ToString();
+            //            else
+            //            {
+            //                sonuc = Math.Round(round, 4);
+            //                dgQuotationAddedItems.Rows[i].Cells[index].Value = sonuc.ToString();
+            //            }
+            //        }
+            //    }
+            //}
 
         }
     }
