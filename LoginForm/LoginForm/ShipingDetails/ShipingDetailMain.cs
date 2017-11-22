a033ffab37145a14222a4dba28a43c2ee9deb14b
Mümin Kurucu <kurucumumin94@gmail.com>
2017-11-22 12:03:47 +0300

    ShipingDetail Main

---
+++ LoginForm/LoginForm/ShipingDetails/ShipingDetailMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm.ShipingDetails
+{
+    public partial class ShipingDetailMain : Form
+    {
+        public ShipingDetailMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
