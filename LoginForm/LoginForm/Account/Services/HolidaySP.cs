ec6ca023c865efe1432accad137656c46d5094fb
Mümin Kurucu <kurucumumin94@gmail.com>
2018-03-16 17:43:16 +0300

    Payroll - frmAttendance formu okey

--- LoginForm/LoginForm/Account/Services/HolidaySP.cs
+++ LoginForm/LoginForm/Account/Services/HolidaySP.cs
@@ -17,7 +17,12 @@ namespace LoginForm.Account.Services
             decimal decResult = 0;
             try
             {
-               if(IME.HolliDayChecking(date)!=null) decResult = Decimal.Parse( IME.HolliDayChecking(date).ToString());
+                if (IME.HolliDayChecking(date) != null)
+                {
+                    int? datee = IME.HolliDayChecking(date).FirstOrDefault();
+                    decResult = Convert.ToDecimal(datee);
+                }
+                  
             }
             catch (Exception ex)
             {
