a713fa0b1020cffb8bd2d7e0ecfbb724ac525b04
nebi13 <30920506+nebi13@users.noreply.github.com>
2018-04-09 19:15:50 +0300

    IME

--- LoginForm/LoginForm/DataSet/SaleOrder.cs
+++ LoginForm/LoginForm/DataSet/SaleOrder.cs
@@ -18,6 +18,7 @@ namespace LoginForm.DataSet
         public SaleOrder()
         {
             this.DeliveryNoteMasters = new HashSet<DeliveryNoteMaster>();
+            this.DeliveryNoteMasters1 = new HashSet<DeliveryNoteMaster>();
             this.PurchaseOrderDetails = new HashSet<PurchaseOrderDetail>();
             this.StockReserves = new HashSet<StockReserve>();
             this.SaleOrderDetails = new HashSet<SaleOrderDetail>();
@@ -70,6 +71,8 @@ namespace LoginForm.DataSet
         public virtual CustomerWorker CustomerWorker2 { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<DeliveryNoteMaster> DeliveryNoteMasters { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<DeliveryNoteMaster> DeliveryNoteMasters1 { get; set; }
         public virtual ExchangeRate ExchangeRate { get; set; }
         public virtual FinancialYear FinancialYear { get; set; }
         public virtual PaymentMethod PaymentMethod { get; set; }
