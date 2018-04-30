3cc6a86717b4b1cdc373c8f11292d5f3025fb093
Mümin Kurucu <kurucumumin94@gmail.com>
2018-04-30 15:44:15 +0300

    Mumin Developer

--- LoginForm/LoginForm/Customer/CustomerMain.designer.cs
+++ LoginForm/LoginForm/Customer/CustomerMain.designer.cs
@@ -88,6 +88,7 @@
             this.label21 = new System.Windows.Forms.Label();
             this.QuoCurrencyType = new System.Windows.Forms.ComboBox();
             this.tab_adresses = new System.Windows.Forms.TabPage();
+            this.CityAdd = new System.Windows.Forms.Button();
             this.TownAdd = new System.Windows.Forms.Button();
             this.cbDefaultInvoiceAdress = new System.Windows.Forms.CheckBox();
             this.cbDefaultDeliveryAdress = new System.Windows.Forms.CheckBox();
@@ -151,7 +152,6 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.tableMain = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.CityAdd = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tab_company.SuspendLayout();
             this.tab_account.SuspendLayout();
@@ -212,7 +212,7 @@
             this.tab_company.Controls.Add(this.label1);
             this.tab_company.Location = new System.Drawing.Point(4, 25);
             this.tab_company.Name = "tab_company";
-            this.tab_company.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tab_company.Padding = new System.Windows.Forms.Padding(3);
             this.tab_company.Size = new System.Drawing.Size(924, 274);
             this.tab_company.TabIndex = 0;
             this.tab_company.Text = "Company Info";
@@ -266,7 +266,7 @@
             // btnAddSubcategory
             // 
             this.btnAddSubcategory.Location = new System.Drawing.Point(310, 162);
-            this.btnAddSubcategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddSubcategory.Margin = new System.Windows.Forms.Padding(2);
             this.btnAddSubcategory.Name = "btnAddSubcategory";
             this.btnAddSubcategory.Size = new System.Drawing.Size(23, 21);
             this.btnAddSubcategory.TabIndex = 56;
@@ -277,7 +277,7 @@
             // btnAddMainCategory
             // 
             this.btnAddMainCategory.Location = new System.Drawing.Point(310, 121);
-            this.btnAddMainCategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddMainCategory.Margin = new System.Windows.Forms.Padding(2);
             this.btnAddMainCategory.Name = "btnAddMainCategory";
             this.btnAddMainCategory.Size = new System.Drawing.Size(23, 21);
             this.btnAddMainCategory.TabIndex = 55;
@@ -553,8 +553,8 @@
             this.tab_account.Controls.Add(this.QuoCurrencyType);
             this.tab_account.Location = new System.Drawing.Point(4, 25);
             this.tab_account.Name = "tab_account";
-            this.tab_account.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tab_account.Size = new System.Drawing.Size(925, 273);
+            this.tab_account.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_account.Size = new System.Drawing.Size(924, 274);
             this.tab_account.TabIndex = 2;
             this.tab_account.Text = "Account";
             this.tab_account.UseVisualStyleBackColor = true;
@@ -824,11 +824,22 @@
             this.tab_adresses.Text = "Adresses";
             this.tab_adresses.UseVisualStyleBackColor = true;
             // 
+            // CityAdd
+            // 
+            this.CityAdd.Location = new System.Drawing.Point(244, 132);
+            this.CityAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.CityAdd.Name = "CityAdd";
+            this.CityAdd.Size = new System.Drawing.Size(23, 21);
+            this.CityAdd.TabIndex = 58;
+            this.CityAdd.Text = "+";
+            this.CityAdd.UseVisualStyleBackColor = true;
+            this.CityAdd.Click += new System.EventHandler(this.CityAdd_Click);
+            // 
             // TownAdd
             // 
             this.TownAdd.Enabled = false;
             this.TownAdd.Location = new System.Drawing.Point(244, 169);
-            this.TownAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TownAdd.Margin = new System.Windows.Forms.Padding(2);
             this.TownAdd.Name = "TownAdd";
             this.TownAdd.Size = new System.Drawing.Size(23, 21);
             this.TownAdd.TabIndex = 57;
@@ -1093,7 +1104,7 @@
             this.tab_contact.Controls.Add(this.btnContactCancel);
             this.tab_contact.Location = new System.Drawing.Point(4, 25);
             this.tab_contact.Name = "tab_contact";
-            this.tab_contact.Size = new System.Drawing.Size(925, 273);
+            this.tab_contact.Size = new System.Drawing.Size(924, 274);
             this.tab_contact.TabIndex = 4;
             this.tab_contact.Text = "Contact";
             this.tab_contact.UseVisualStyleBackColor = true;
@@ -1470,9 +1481,9 @@
             this.tableMain.Controls.Add(this.panel1, 0, 1);
             this.tableMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableMain.Location = new System.Drawing.Point(0, 0);
-            this.tableMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableMain.Margin = new System.Windows.Forms.Padding(2);
             this.tableMain.Name = "tableMain";
-            this.tableMain.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableMain.Padding = new System.Windows.Forms.Padding(4);
             this.tableMain.RowCount = 3;
             this.tableMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 309F));
             this.tableMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
@@ -1489,21 +1500,10 @@
             this.panel1.Controls.Add(this.Search);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(6, 315);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(934, 57);
             this.panel1.TabIndex = 2;
-            // 
-            // CityAdd
-            // 
-            this.CityAdd.Location = new System.Drawing.Point(244, 132);
-            this.CityAdd.Margin = new System.Windows.Forms.Padding(2);
-            this.CityAdd.Name = "CityAdd";
-            this.CityAdd.Size = new System.Drawing.Size(23, 21);
-            this.CityAdd.TabIndex = 58;
-            this.CityAdd.Text = "+";
-            this.CityAdd.UseVisualStyleBackColor = true;
-            this.CityAdd.Click += new System.EventHandler(this.CityAdd_Click);
             // 
             // CustomerMain
             // 
@@ -1514,7 +1514,7 @@
             this.KeyPreview = true;
             this.MinimumSize = new System.Drawing.Size(734, 588);
             this.Name = "CustomerMain";
-            this.Text = "Customer Card";
+            this.Text = " ";
             this.TopMost = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.CustomerMain_Load);
